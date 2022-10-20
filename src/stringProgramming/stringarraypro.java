9efefdfe6dd28b5d75065a5e63327f7ab859f505
jwala006 <110559813+jwala006@users.noreply.github.com>
2022-10-21 01:21:27 +0530

    second chekin

---
+++ src/stringProgramming/stringarraypro.java
@@ -0,0 +1,14 @@
+package stringProgramming;
+
+public class stringarraypro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ char ch;
+ for(ch='A';ch<='Z';ch++) {
+	
+	 System.out.println(ch);
+	 }
+
+}
+}
